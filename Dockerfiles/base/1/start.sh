1694bf6f762d068ffce23ce0ce96aad98aa48c92
Saurabh Kumar <saurabhgeek92@gmail.com>
2015-01-05 15:37:23 +0530

    Add passwordless sudo
    Add passwordless sudo.
    Also fix locales.

--- Dockerfiles/base/1/start.sh
+++ Dockerfiles/base/1/start.sh
@@ -3,7 +3,9 @@
 if id -u $1 >/dev/null 2>&1; then
     echo "user exists"
 else
-    adduser --quiet --disabled-password --gecos "" $1 --uid $2 >/dev/null 2>&1
+    [ $(getent group wheel) ] || groupadd wheel
+    adduser --quiet --disabled-password --gecos "" $1 --uid $2 --gid $3 >/dev/null 2>&1
+    gpasswd -a $1 wheel >/dev/null 2>&1
 fi
 
-su -s "$3" $1
+su -s "$4" - $1
